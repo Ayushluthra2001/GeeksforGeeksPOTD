5eb7e1e05cba3b918264569e91b8c86a5e4d3203
Ayush Luthra <71180629+Ayushluthra2001@users.noreply.github.com>
2022-09-25 19:06:43 +0530

    Create Queries on a Matrix.cpp

---
+++ Queries on a Matrix.cpp
@@ -0,0 +1,37 @@
+
+class Solution {
+  public:
+    vector<vector<int>> solveQueries(int n, vector<vector<int>> Queries) {
+
+        
+
+        vector<vector<int>> ans(n,vector<int>(n,0));
+
+        
+
+        for(auto t:  Queries){
+
+            int a=t[0] ;
+
+            int b=t[1] ;
+
+            int c=t[2] ;
+
+            int d=t[3] ;
+
+            for(int j=a;j<=c;j++){
+
+                for(int k=b;k<=d;k++){
+
+                    ans[j][k]++;
+
+                }
+
+            }
+
+        }
+
+        return ans;
+
+    }
+};
