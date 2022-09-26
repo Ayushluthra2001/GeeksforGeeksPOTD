d6d156be2293fcbd17d99da0a119d4b457eddbfe
Ayush Luthra <71180629+Ayushluthra2001@users.noreply.github.com>
2022-09-26 14:25:59 +0530

    Create Tom and Jerry.cpp

---
+++ Tom and Jerry.cpp
@@ -0,0 +1,15 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int numsGame(int N) {
+        // code here
+        if(N==0) return 0;
+        if(N%2==0) return 1;
+        return 0;
+    }
+};
+
