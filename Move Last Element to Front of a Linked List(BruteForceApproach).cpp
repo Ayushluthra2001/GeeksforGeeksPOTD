43779bb21570c32a6cdbbf650e567d7028505673
Ayush Luthra <71180629+Ayushluthra2001@users.noreply.github.com>
2022-10-16 09:56:31 +0530

    Create Move Last Element to Front of a Linked List(BruteForceApproach).cpp

---
+++ Move Last Element to Front of a Linked List(BruteForceApproach).cpp
@@ -0,0 +1,72 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class ListNode{
+public:
+    int val;
+    ListNode *next;
+    ListNode(int x){
+        val=x;
+        next=NULL;
+    }
+};
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+public:
+    ListNode *moveToFront(ListNode *head){
+        vector<int>ans;
+        ListNode* temp= head;
+        while(temp!=NULL){
+            ans.push_back(temp->val);
+            temp=temp->next;
+            
+        }
+        ListNode* node=new ListNode(ans[ans.size()-1]);
+        temp= node;
+        for(int i=0;i<ans.size()-1;i++){
+            temp->next=new ListNode(ans[i]);
+            temp=temp->next;
+        }
+        return node;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<ListNode*> a(n);
+        for(int i=0;i<n;i++){
+            int x;
+            cin>>x;
+            a[i]=new ListNode(x);
+            if(i!=0){
+                a[i-1]->next=a[i];
+            }
+        }
+        ListNode *head=a[0];
+        Solution ob;
+        head=ob.moveToFront(head);
+        while(head!=NULL){
+            cout<<head->val;
+            if(head->next!=NULL){
+                cout<<" ";
+            }
+            head=head->next;
+        }
+        cout<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
