0de35fe3f6561b6228daca16fc6258cd079992e6
Ayush Luthra <71180629+Ayushluthra2001@users.noreply.github.com>
2022-10-16 10:04:41 +0530

    Create Move Last Element to Front of a Linked List(OptimizeApproach).cpp

---
+++ Move Last Element to Front of a Linked List(OptimizeApproach).cpp
@@ -0,0 +1,69 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class ListNode{
+public:
+    int val;
+    ListNode *next;
+    ListNode(int x){
+        val=x;
+        next=NULL;
+    }
+};
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+public:
+    ListNode *moveToFront(ListNode *head){
+        if(head->next==NULL) return head;
+        ListNode* temp=head;
+        while(temp->next->next!=NULL){
+            temp=temp->next;
+        }
+        ListNode* temp2=temp->next;
+        temp->next=NULL;
+        temp2->next=head;
+        head=temp2;
+        
+        return head;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<ListNode*> a(n);
+        for(int i=0;i<n;i++){
+            int x;
+            cin>>x;
+            a[i]=new ListNode(x);
+            if(i!=0){
+                a[i-1]->next=a[i];
+            }
+        }
+        ListNode *head=a[0];
+        Solution ob;
+        head=ob.moveToFront(head);
+        while(head!=NULL){
+            cout<<head->val;
+            if(head->next!=NULL){
+                cout<<" ";
+            }
+            head=head->next;
+        }
+        cout<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
