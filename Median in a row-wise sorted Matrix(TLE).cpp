3c02e0a8816faeadfd96a73c09ee47c501eeb9ab
Ayush Luthra <71180629+Ayushluthra2001@users.noreply.github.com>
2022-11-01 00:35:11 +0530

    Create Median in a row-wise sorted Matrix(TLE).cpp

---
+++ Median in a row-wise sorted Matrix(TLE).cpp
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution{   
+public:
+    int median(vector<vector<int>> &matrix, int R, int C){
+        // code here   
+        vector<int>ans;
+        for(int i=0;i<R;i++){
+            for(int j=0;j<C;j++){
+                ans.push_back(matrix[i][j]);
+            }
+        }
+        sort(ans.begin(),ans.end());
+        int n=ans.size();
+        return ans[n/2];
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int r, c;
+        cin>>r>>c;
+        vector<vector<int>> matrix(r, vector<int>(c));
+        for(int i = 0; i < r; ++i)
+            for(int j = 0;j < c; ++j)
+                cin>>matrix[i][j];
+        Solution obj;
+        cout<<obj.median(matrix, r, c)<<endl;        
+    }
+    return 0;
+}
+// } Driver Code Ends
