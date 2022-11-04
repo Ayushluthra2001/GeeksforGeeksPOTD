811f0715c5a8c1cc08a18f87e5d826c514b3d2b1
Ayush Luthra <71180629+Ayushluthra2001@users.noreply.github.com>
2022-11-04 13:23:07 +0530

    Create Base Equivalence.cpp

---
+++ Base Equivalence.cpp
@@ -0,0 +1,41 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+    string baseEquiv(int n, int m){
+        // code here
+        int count,temp;
+        for(int i=2;i<=32;i++){
+            count=m;
+            temp=n;
+             while(temp>0){
+            temp=temp/i;
+            count--;
+        }
+        if(count==0) return "Yes";
+        }
+       
+        
+        return "No";
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, m;
+        cin >> n >> m;
+        Solution ob;
+        cout<<ob.baseEquiv(n,m)<<endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
