e9a053069a1ce7c6e9e2e6a7f65d5baa136a3fb9
Ayush Luthra <71180629+Ayushluthra2001@users.noreply.github.com>
2022-12-01 00:56:50 +0530

    Create Rearrange Array Alternately.cpp

---
+++ Rearrange Array Alternately.cpp
@@ -0,0 +1,76 @@
+//{ Driver Code Starts
+// C++ program to rearrange an array in minimum 
+// maximum form 
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+    public:
+    // This function wants you to modify the given input
+    // array and no need to return anything
+    // arr: input array
+    // n: size of array
+    //Function to rearrange  the array elements alternately.
+    void rearrange(long long *arr, int n) 
+    { 
+    	
+    	// Your code here
+    	vector<long long>arr2;
+        int i=0;
+        int j=n-1;
+        while(i<=j){
+            if(i!=j){
+                arr2.push_back(arr[j]);
+                arr2.push_back(arr[i]);
+            }else{
+                arr2.push_back(arr[i]);
+            }
+            i++;
+            j--;
+        }
+    	for(int i=0;i<n;i++){
+    	    arr[i]=arr2[i];
+    	}
+    }
+};
+
+//{ Driver Code Starts.
+
+// Driver program to test above function 
+int main() 
+{
+    int t;
+    
+    //testcases
+    cin >> t;
+    
+    while(t--){
+        
+        //size of array
+        int n;
+        cin >> n;
+        
+        long long arr[n];
+        
+        //adding elements to the array
+        for(int i = 0;i<n;i++){
+            cin >> arr[i];
+        }
+        
+        Solution ob;
+        
+        //calling rearrange() function
+        ob.rearrange(arr, n);
+        
+        //printing the elements
+        for (int i = 0; i < n; i++) 
+		    cout << arr[i] << " ";
+		
+		cout << endl;
+    }
+	return 0; 
+} 
+
+// } Driver Code Ends
