aa7ff2ace6118a3706c22760bd0d2ea238a26320
Ayush Luthra <71180629+Ayushluthra2001@users.noreply.github.com>
2022-12-17 19:40:21 +0530

    Create Break a number.cpp

---
+++ Break a number.cpp
@@ -0,0 +1,38 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+//User function Template for C++
+class Solution{   
+public:
+    int waysToBreakNumber(int N){
+        
+       
+        long mod=1000000007;
+
+        long c=N+1;
+
+        long ans=((c*(c+1))/2)%mod;
+
+        return (int)ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N;
+        cin >> N;
+        Solution ob;
+        cout << ob.waysToBreakNumber(N) << endl;
+    }
+    return 0; 
+} 
+// } Driver Code Ends
