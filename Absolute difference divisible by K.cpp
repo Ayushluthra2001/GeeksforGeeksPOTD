d12f8672d22b68c689ba08c9fb047a34c576b942
Ayush Luthra <71180629+Ayushluthra2001@users.noreply.github.com>
2023-01-08 13:14:33 +0530

    Create Absolute difference divisible by K.cpp

---
+++ Absolute difference divisible by K.cpp
@@ -0,0 +1,43 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution {
+  public:
+    long long countPairs(int n, int arr[], int k) {
+        // code here
+        int count=0;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if(abs(arr[j]-arr[i])%k==0) count++;
+            }
+        }
+        return count;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n,k;
+        cin>>n;
+        
+        int arr[n];
+        for(int i=0; i<n; i++)
+            cin>>arr[i];
+        
+        cin>>k;
+
+        Solution ob;
+        cout << ob.countPairs(n,arr,k) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
