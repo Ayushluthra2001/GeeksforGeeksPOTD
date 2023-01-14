abc2b2a1fa4064ecf45edd4f9c64278152ba9962
Ayush Luthra <71180629+Ayushluthra2001@users.noreply.github.com>
2023-01-15 00:09:09 +0530

    Create Count the Substring(O(N*N*N)).cpp

---
+++ Count the Substring(O(N*N*N)).cpp
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+//Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+class Solution{
+public:
+  long long countSubstring(string S){
+    // code here
+    int count=0;
+    for(int i=0;i<S.length();i++){
+        for(int j=i;j<S.length();j++){
+            int one=0;
+            int zero=0;
+            for(int k=i;k<=j;k++){
+                if(S[k]=='1')  one++;
+                else zero++;
+            }
+          
+            if(one>zero) count++;
+        }
+    }
+    return count;
+  }
+};
+
+//{ Driver Code Starts.
+int main() {
+ ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL); 
+ int t=1;
+ cin>>t;
+ for(int i=1;i<=t;i++){
+    string S;
+    cin>>S;
+    Solution obj;
+    long long ans =obj.countSubstring(S);
+    cout<<ans<<endl;
+ }
+}
+// } Driver Code Ends
