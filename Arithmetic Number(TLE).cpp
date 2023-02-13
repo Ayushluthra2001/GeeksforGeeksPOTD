24a203e3858a3cb23691455073e9a70e2a601ec3
Ayush Luthra <71180629+Ayushluthra2001@users.noreply.github.com>
2023-02-13 14:17:46 +0530

    Create Arithmetic Number(TLE).cpp

---
+++ Arithmetic Number(TLE).cpp
@@ -0,0 +1,38 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+    int inSequence(int A, int B, int C){
+        // code here
+        int s=A;
+        while(abs(s)<=abs(B)){
+            s+=C;
+            if(s==B) return true;
+            if(s>B) return false;
+        }
+        return false;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int A, B, C;
+        cin>>A>>B>>C;
+        
+        Solution ob;
+        cout<<ob.inSequence(A, B, C)<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
