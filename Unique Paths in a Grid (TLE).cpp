75b9cb903f859fec6e570782515a3768fcdd7e90
Ayush Luthra <71180629+Ayushluthra2001@users.noreply.github.com>
2023-02-23 00:14:26 +0530

    Create Unique Paths in a Grid (TLE).cpp

---
+++ Unique Paths in a Grid (TLE).cpp
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution {
+  public:
+  # define mod 1000000007
+    int solve(vector<vector<int>>&vis , vector<vector<int>>&grid,int n , int m, int i , int j){
+      if(i>=n || j>=m || i<0 || j<0 || grid[i][j]==0 ) return 0;
+      if(i==n-1 && j==n-1 && grid[i][j]==1) return 1;
+      
+      int down=solve(vis,grid,n,m,i+1,j)%mod;
+      int right=solve(vis,grid,n,m,i,j+1)%mod;
+      return (down+right)%mod;
+    }
+    int uniquePaths(int n, int m, vector<vector<int>> &grid) {
+        // code here
+        if(grid[n-1][m-1]==0) return 0;
+        vector<vector<int>>vis(n+1,vector<int>(m+1,false));
+        
+        return solve(vis,grid,n,m,0,0);
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n,m,x;
+        cin>>n>>m;
+        
+        vector<vector<int>> grid(n,vector<int>(m));
+        
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                cin>>grid[i][j];
+            }
+        }
+
+        Solution ob;
+        cout << ob.uniquePaths(n,m,grid) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
