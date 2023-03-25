0fde32509db320ea13d55ed46f20568126a7b56f
Ayush Luthra <71180629+Ayushluthra2001@users.noreply.github.com>
2023-03-26 00:27:13 +0530

    Create Frogs and Jumps( Brute Force Approach).cpp

---
+++ Frogs and Jumps( Brute Force Approach).cpp
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution {
+  public:
+    int unvisitedLeaves(int N, int leaves, int frogs[]) {
+        // Code here
+        vector<int>vis(leaves+1,0);
+        int count=0;
+        for(int i=0;i<N;i++){
+            for(int j=frogs[i];j<=leaves;j+=frogs[i]){
+                vis[j]=1;
+            }
+        }
+       
+        for(int i=1;i<=leaves;i++){
+            if(vis[i]==false) count++;
+        }
+        return count;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N, leaves;
+        cin >> N >> leaves;
+        int frogs[N];
+        for (int i = 0; i < N; i++) {
+            cin >> frogs[i];
+        }
+
+        Solution ob;
+        cout << ob.unvisitedLeaves(N, leaves, frogs) << endl;
+    }
+}
+// } Driver Code Ends
