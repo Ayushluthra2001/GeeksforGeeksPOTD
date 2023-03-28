1c0b7a95184efeccfcfa109a65aea85577dc9a3b
Ayush Luthra <71180629+Ayushluthra2001@users.noreply.github.com>
2023-03-29 00:31:59 +0530

    Create Count the Substrings.cpp

---
+++ Count the Substrings.cpp
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+//Initial Template for C++
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+class Solution
+{
+    public:
+    int countSubstring(string S)
+    {
+        // code here
+        int count=0;
+        for(int i=0;i<S.length();i++){
+             int countLower=0;
+                int countUpper=0;
+            for(int j=i;j<S.length();j++){
+                
+                
+                    // cout<<S[k];
+                   
+                    if(islower(S[j])) countLower++;
+                    else countUpper++;
+                   
+                    
+                
+                if(countLower==countUpper) count++;
+                //   cout<<countLower<<" "<<countUpper<<endl;
+                // cout<<endl;
+                
+            }
+        }
+        return count;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string S;
+        cin>>S;
+        Solution obj;
+        cout<<obj.countSubstring(S)<<endl;
+    }
+}  
+// } Driver Code Ends
