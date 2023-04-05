ab2e2f4296d1e2a8f75f29be6ac6cefe62a8e343
Ayush Luthra <71180629+Ayushluthra2001@users.noreply.github.com>
2023-04-05 13:09:00 +0530

    Create Count Special Numbers.cpp

---
+++ Count Special Numbers.cpp
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    int countSpecialNumbers(int N, vector<int> arr) {
+        // Code here
+        int count=0;
+        for(int i=0;i<N;i++){
+            bool check=false;
+            for(int j=0;j<N;j++){
+                
+                if(i!=j && arr[i]%arr[j]==0) {
+                   check=true;
+                   break;
+                }
+            }
+            if(check) count++;
+        }
+        
+        return count;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin >> N;
+        vector<int> arr(N);
+        for (int i = 0; i < N; i++) {
+            cin >> arr[i];
+        }
+
+        Solution ob;
+        cout << ob.countSpecialNumbers(N, arr) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
