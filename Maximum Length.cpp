a5986384e84fb52d4a4af6ecd7eef222b48cae4c
Ayush Luthra <71180629+Ayushluthra2001@users.noreply.github.com>
2023-04-11 00:17:33 +0530

    Create Maximum Length.cpp

---
+++ Maximum Length.cpp
@@ -0,0 +1,38 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+public:
+    int solve(int a, int b, int c) {
+        int minn = min(a,min(b,c));
+        int maxx = max(a,max(b,c));
+        int midd = a+b+c - maxx - minn;
+        if(maxx>(minn + midd)*2) return -1;
+        return a+b+c;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        int a, b, c;
+        cin >> a >> b >> c;
+
+        Solution ob;
+        int ans = ob.solve(a, b, c);
+     
+
+        cout <<  ans << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
