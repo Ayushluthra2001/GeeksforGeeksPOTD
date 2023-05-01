b79dfc8ed6113387ad26068e5a552473f283428d
Ayush Luthra <71180629+Ayushluthra2001@users.noreply.github.com>
2023-05-01 20:43:16 +0530

    Create n-TREE.cpp

---
+++ n-TREE.cpp
@@ -0,0 +1,109 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node{
+public:
+    int data;
+    vector<Node*> children;
+    Node(int val){
+        data=val;
+    }
+};
+
+class N_ary_Tree{
+public:
+    Node *root;
+    int size;
+    N_ary_Tree(){
+        root=NULL;
+        size=0;
+    }
+    Node *add(int new_key,Node *parent_key=NULL){
+        Node *new_node = new Node(new_key);
+        if(parent_key == NULL){
+            root = new_node;
+            size = 1;
+        }
+        else{
+            parent_key->children.push_back(new_node);
+            size += 1;
+        }
+        return new_node;
+    }
+};
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+public:
+    string dfs(Node* root,unordered_map<string,int> &ump,int &ans){
+         if(!root){
+             return "#";
+         }
+        string str="";
+        for(auto x:root->children){
+            str += dfs(x,ump,ans) + ',';
+        }
+         string s = str + to_string(root->data);
+         if(ump[s]==1){
+             ans++;
+         }
+         ump[s]++;
+         return s;
+    }
+    int duplicateSubtreeNaryTree(Node *root){
+        // Code here
+        int ans=0;
+        unordered_map<string,int> ump;
+        dfs(root,ump,ans);
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    for(int i=0;i<t;i++){
+        if(i==0){
+            string p;
+            getline(cin,p);
+        }
+        string str;
+        getline(cin,str);
+        stringstream st(str);
+        vector<string> s;
+        string y;
+        while(st>>y){
+            s.push_back(y);
+        }
+        N_ary_Tree *tree = new N_ary_Tree();
+        Node *curr;
+        queue<Node*> q;
+        for(int i=0;i<s.size();i++){
+            if (i == 0){
+                curr=tree->add(stoi(s[0]));
+            }
+            else if(s[i] == " "){
+                continue;
+            }
+            else if(q.size() and s[i] == "N"){
+                curr = q.front();
+                q.pop();
+            }
+            else if(s[i] != "N"){
+                q.push(tree->add(stoi(s[i]), curr));
+            }
+        }
+        Solution ob;
+        int res = ob.duplicateSubtreeNaryTree(tree->root);
+        cout<<res<<endl;
+    }
+}
+// } Driver Code Ends
