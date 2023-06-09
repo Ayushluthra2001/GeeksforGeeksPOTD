ccb45633d5b6bd8f338bdae0b95b2b6330d2173d
Ayush Luthra <71180629+Ayushluthra2001@users.noreply.github.com>
2023-06-09 23:00:40 +0530

    Create Permutations of a given string.cpp

---
+++ Permutations of a given string.cpp
@@ -0,0 +1,39 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+	public:
+		vector<string>find_permutation(string s)
+{sort(s.begin(),s.end());
+   vector<string> ans;
+   ans.push_back(s);
+   while(next_permutation(s.begin(),s.end()))
+   ans.push_back(s);
+   return ans;}
+};
+
+
+
+//{ Driver Code Starts.
+int main(){
+    int t;
+    cin >> t;
+    while(t--)
+    {
+	    string S;
+	    cin >> S;
+	    Solution ob;
+	    vector<string> ans = ob.find_permutation(S);
+	    for(auto i: ans)
+	    {
+	    	cout<<i<<" ";
+	    }
+	    cout<<"\n";
+    }
+	return 0;
+}
+
+// } Driver Code Ends
