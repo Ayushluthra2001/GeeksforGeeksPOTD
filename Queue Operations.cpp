394a94a75be689acbcc1612fbf4bce12db8bf8ee
Ayush Luthra <71180629+Ayushluthra2001@users.noreply.github.com>
2023-06-17 22:59:08 +0530

    Create Queue Operations.cpp

---
+++ Queue Operations.cpp
@@ -0,0 +1,76 @@
+//{ Driver Code Starts
+//Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+// Helper class Solution to implement 
+// insert() and findFrequency()
+class Solution{
+    public:
+    // Function to insert element into the queue
+    // map<int,int>um;
+    void insert(queue<int> &q, int k){
+        // Your code here
+        q.push(k);
+    }
+    
+    // Function to find frequency of an element
+    // return the frequency of k
+    int findFrequency(queue<int> &q, int k){
+        queue<int>temp  = q;
+        int count = 0;
+        while(temp.size()!=0){
+            int x = temp.front();
+            temp.pop();
+            if(x==k){
+                count++;
+            }
+        }
+        if(count==0){
+            return -1;
+        }
+        else{
+            return count;
+        }
+    }
+    
+};
+
+//{ Driver Code Starts.
+int main() {
+    
+		int testcase;
+		cin>>testcase;
+		
+		while(testcase-- > 0){
+		    // Declaring Queue
+		    queue<int> q;
+		    int n, k;
+		    cin>>n;
+		    
+		    // Invoking object of Geeks class
+		    Solution obj;
+		    
+		    for(int i = 0;i<n;i++){
+		        cin >> k;
+		        obj.insert(q, k);
+		    }
+		   
+		    int m;
+		    cin >> m;
+		    for(int i = 0;i<m;i++){
+		        cin >> k;
+		        int f = obj.findFrequency(q, k);
+		        if(f != 0){
+		            cout<<f<<endl;
+		        }
+		        else{
+		            cout<<"-1\n";
+		        }
+		    }
+		}
+	return 0;
+}
+// } Driver Code Ends
