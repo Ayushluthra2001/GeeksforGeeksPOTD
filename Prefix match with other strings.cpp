6697e9edb9df162498cdb5bb901b85498dc1a16f
Ayush Luthra <71180629+Ayushluthra2001@users.noreply.github.com>
2023-06-24 16:29:58 +0530

    Create Prefix match with other strings.cpp

---
+++ Prefix match with other strings.cpp
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{   
+public:
+    int klengthpref(string arr[], int n, int k, string str){    
+        int count=0;
+        for(int i=0;i<n;i++){
+            int j=0;
+            int m=k;
+            while(j<arr[i].length() && j<str.length() && m>0 && str[j]==arr[i][j]) j++,m--;
+            if(m==0) count++;
+        }
+        return count;
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        string arr[n];
+        for(int i=0;i<n;i++)
+        {
+            string s;
+            cin>>arr[i];
+            
+        }
+        int k;
+        cin>>k;
+        string str;
+        cin>>str;
+        
+        Solution ob;
+        cout << ob.klengthpref(arr, n, k, str) << endl;
+    }
+    return 0; 
+} 
+
+// } Driver Code Ends
