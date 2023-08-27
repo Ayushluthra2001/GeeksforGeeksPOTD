383a1da54a8cf5379c8252262cb50b7ff1226e08
Ayush Luthra <71180629+Ayushluthra2001@users.noreply.github.com>
2023-08-27 15:46:34 +0530

    Create Reverse a String.cpp

---
+++ Reverse a String.cpp
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+// } Driver Code Ends
+//User function Template for C++
+class Solution
+{
+    public:
+    string reverseWord(string str)
+    {
+        // Your code goes here
+        int start=0;
+        int end=str.length()-1;
+        while(start<=end){
+            char ch =str[start];
+            str[start]=str[end];
+            str[end]=ch;
+            start++;
+            end--;
+        }
+        return str;
+    }
+};
+
+
+
+//{ Driver Code Starts.
+
+int main() {
+	
+	int t;
+	cin>>t;
+	while(t--)
+	{
+    	string s;
+    	cin >> s;
+    	Solution ob;
+    	cout << ob.reverseWord(s) << endl;
+	}
+	return 0;
+	
+}
+
+
+// } Driver Code Ends
