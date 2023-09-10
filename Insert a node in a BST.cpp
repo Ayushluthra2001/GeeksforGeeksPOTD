6ea811ce4a225e7f0c954b9b215d2ec90a7e0b9d
Ayush Luthra <71180629+Ayushluthra2001@users.noreply.github.com>
2023-09-10 20:46:51 +0530

    Create Insert a node in a BST.cpp

---
+++ Insert a node in a BST.cpp
@@ -0,0 +1,172 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+#define MAX_HEIGHT 100000
+
+// Tree Node
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+
+// } Driver Code Ends
+// Function to insert a node in a BST.
+
+/*
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+*/
+
+class Solution
+{
+    public:
+    void solve(Node* node, vector<int>&d){
+        if(node==NULL) return ;
+        solve(node->left,d);
+        d.push_back(node->data);
+        solve(node->right,d);
+    }
+    Node* solve2(vector<int>&d,int start,int end){
+        if(start>end) return NULL;
+        int mid=start+(end-start)/2;
+        Node* ans=new Node(d[mid]);
+        ans->left=solve2(d,start,mid-1);
+        ans->right=solve2(d,mid+1,end);
+        return  ans;
+    }
+        Node* insert(Node* node, int data) {
+        
+            // Your code goes here
+            vector<int>d;
+            solve(node,d);
+            d.push_back(data);
+            sort(d.begin(),d.end());
+            
+            vector<int>dd;
+            Node* ans= solve2(d,0,d.size()-1);
+            
+            
+            node=ans;
+            solve(node,dd);
+            // for(auto i : dd){
+            //     cout<<i<<" ";
+            // }
+            // cout<<endl;
+            return node;
+    }
+
+};
+
+
+//{ Driver Code Starts.
+
+// Function to Build Tree
+Node* buildTree(string str) {
+    // Corner Case
+    if (str.length() == 0 || str[0] == 'N') return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for (string str; iss >> str;) ip.push_back(str);
+
+    // Create the root of the tree
+    Node* root = new Node(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while (!queue.empty() && i < ip.size()) {
+
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+
+        // Get the current node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if (currVal != "N") {
+
+            // Create the left child for the current node
+            currNode->left = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if (i >= ip.size()) break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if (currVal != "N") {
+
+            // Create the right child for the current node
+            currNode->right = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+void inorder(Node* root, vector<int>& v) {
+    if (root == NULL) return;
+
+    inorder(root->left, v);
+    v.push_back(root->data);
+    inorder(root->right, v);
+}
+
+int main() {
+
+    int t;
+    string tc;
+    getline(cin, tc);
+    t = stoi(tc);
+    while (t--) {
+        string s;
+        getline(cin, s);
+        Node* root = buildTree(s);
+
+        getline(cin, s);
+        int k = stoi(s);
+        // getline(cin, s);
+        Solution ob;
+        ob.insert(root, k);
+        vector<int> v;
+        inorder(root, v);
+        for (int i = 0; i < v.size(); i++) cout << v[i] << " ";
+        cout << endl;
+
+        // cout<<"~"<<endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
