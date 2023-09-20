5d04095650879a3ce7dc8d7b012a5c41c5e41b09
Ayush Luthra <71180629+Ayushluthra2001@users.noreply.github.com>
2023-09-20 10:24:15 +0530

    Create Rotate Bits.cpp

---
+++ Rotate Bits.cpp
@@ -0,0 +1,31 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution {
+public:
+	vector<int> rotate(int n, int d) {
+		d %= 16;
+		return {((n << d) | (n >> (16 - d))) & 65535,
+			    ((n >> d) | (n << (16 - d))) & 65535};
+	}
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t; cin >> t;
+    while (t--)
+    {
+        
+        int n, d; cin >> n >> d;
+        Solution ob;
+        vector <int> res = ob.rotate (n, d);
+        cout << res[0] << endl << res[1] << endl;
+    }
+}
+// Contributed By: Pranay Bansal
+
+// } Driver Code Ends
