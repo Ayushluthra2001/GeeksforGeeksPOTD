51210670a3e02ebf04121efa147e9b1eb32747da
Ayush Luthra <71180629+Ayushluthra2001@users.noreply.github.com>
2023-10-13 19:53:36 +0530

    Create Floor in BST.cpp

---
+++ Floor in BST.cpp
@@ -0,0 +1,74 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct Node {
+    int data;
+    Node *right;
+    Node *left;
+
+    Node(int x) {
+        data = x;
+        right = NULL;
+        left = NULL;
+    }
+};
+
+
+// } Driver Code Ends
+// Function to search a node in BST.
+class Solution{
+
+public:
+    void solve(Node* root, int x, int &maxi){
+        if(root==NULL) return ;
+        solve(root->left,x,maxi);
+        if(root->data<=x)maxi=root->data;
+        solve(root->right,x,maxi);
+    }
+    int floor(Node* root, int x) {
+        // Code here
+        int maxi=-1;
+        solve(root,x,maxi);
+        return maxi;
+    }
+};
+
+//{ Driver Code Starts.
+
+Node *insert(Node *tree, int val) {
+    Node *temp = NULL;
+    if (tree == NULL) return new Node(val);
+
+    if (val < tree->data) {
+        tree->left = insert(tree->left, val);
+    } else if (val > tree->data) {
+        tree->right = insert(tree->right, val);
+    }
+
+    return tree;
+}
+
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        Node *root = NULL;
+
+        int N;
+        cin >> N;
+        for (int i = 0; i < N; i++) {
+            int k;
+            cin >> k;
+            root = insert(root, k);
+        }
+
+        int s;
+        cin >> s;
+        Solution obj;
+        cout << obj.floor(root, s) << "\n";
+    }
+}
+
+// } Driver Code Ends
