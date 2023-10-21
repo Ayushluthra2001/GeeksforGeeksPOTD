04dd803d8fadf017cabb4e0dd324402e8837bfde
Ayush Luthra <71180629+Ayushluthra2001@users.noreply.github.com>
2023-10-21 14:28:44 +0530

    Create Sum of all divisors from 1 to n(TLE).cpp

---
+++ Sum of all divisors from 1 to n.cpp
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+//User function Template for C++
+class Solution
+{
+public:
+    long long sumOfDivisors(int N)
+    {
+        // Write Your Code here
+        long long ans=0;
+        for(int i=1;i<=N;i++){
+            int count=0;
+            for(int j=1;j<=i;j++){
+                if(i%j==0) count+=j;
+            }
+            ans+=count;
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int N;
+        cin>>N;
+        Solution ob;
+        long long ans  = ob.sumOfDivisors(N);
+        cout<<ans<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
