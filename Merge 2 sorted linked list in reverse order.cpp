2c14ccdd7e36a0f2e9fd6af5cbbff3ba6011c9a9
Ayush Luthra <71180629+Ayushluthra2001@users.noreply.github.com>
2024-01-08 11:17:54 +0530

    Create Merge 2 sorted linked list in reverse order.cpp

---
+++ Merge 2 sorted linked list in reverse order.cpp
@@ -0,0 +1,132 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+/* Link list Node */
+struct Node
+{
+    int data;
+    struct Node* next;
+};
+
+void print(struct Node *Node)
+{
+    while (Node!=NULL)
+    {
+        cout << Node->data << " ";
+        Node = Node->next;
+    }
+}
+
+
+Node *newNode(int data)
+{
+    Node *temp = new Node;
+    temp->data = data;
+    temp->next = NULL;
+    return temp;
+}
+
+
+// } Driver Code Ends
+/*
+
+The structure of linked list is the following
+
+struct Node
+{
+    int data;
+    Node* next;
+};
+
+*/
+class Solution
+{
+    public:
+    void solve(vector<int>&data,Node* node){
+        if(node==NULL) return ;
+        Node* temp=node;
+        while(temp!=NULL){
+            data.push_back(temp->data);
+            temp=temp->next;
+        }
+        return;
+    }
+    
+    struct Node * mergeResult(Node *node1,Node *node2)
+    {
+        // your code goes here
+        vector<int>data;
+        solve(data,node1);
+        solve(data,node2);
+        sort(data.begin(),data.end(),greater<int>());
+        Node* dummy=newNode(-1);
+        Node* temp=dummy;
+        for(int i=0;i<data.size();i++){
+            temp->next= newNode(data[i]);
+            temp=temp->next;
+        }
+        return dummy->next;
+    }  
+};
+
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int T;
+    cin>>T;
+    while(T--)
+    {
+        int nA;
+        cin>>nA;
+        int nB;
+        cin>>nB;
+
+        struct Node* headA=NULL;
+        struct Node* tempA = headA;
+
+        for(int i=0;i<nA;i++)
+        {
+            int ele;
+            cin>>ele;
+            if(headA==NULL)
+            {
+                headA=tempA=newNode(ele);
+
+            }else{
+                tempA->next = newNode(ele);
+				tempA=tempA->next;
+            }
+        }
+
+        struct Node* headB=NULL;
+        struct Node* tempB = headB;
+
+
+        for(int i=0;i<nB;i++)
+        {
+            int ele;
+            cin>>ele;
+            if(headB==NULL)
+            {
+                headB=tempB=newNode(ele);
+
+            }else{
+                tempB->next = newNode(ele);
+				tempB=tempB->next;
+            }
+        }
+        
+        Solution ob;
+        struct Node* result = ob.mergeResult(headA,headB);
+
+        print(result);
+        cout<<endl;
+
+
+    }
+}
+
+// } Driver Code Ends
