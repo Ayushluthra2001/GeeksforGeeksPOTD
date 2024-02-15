07860c5781c1e9a95dd1787bc0c81b5281a0528d
Ayush Luthra <71180629+Ayushluthra2001@users.noreply.github.com>
2024-02-15 23:09:48 +0530

    Create Count all Possible Path.cpp

---
+++ Count all Possible Path.cpp
@@ -0,0 +1,43 @@
+//{ Driver Code Starts
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution {
+    public:
+	int isPossible(vector<vector<int>>paths){
+	    for(int i=0;i<paths.size();i++)
+	    {
+	        int cnt=0;
+	        for(int j=0;j<paths[0].size();j++)
+	        cnt+=paths[i][j];
+	        
+	        if(cnt&1)
+	        return 0;
+	    }
+	    return 1;
+	}
+
+};
+
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int n;
+		cin >> n;
+		vector<vector<int>>paths(n, vector<int>(n, 0));
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < n; j++)
+				cin >> paths[i][j];
+		}
+		Solution obj;
+		int ans = obj.isPossible(paths);
+		cout << ans <<"\n";
+	}
+	return 0;
+}
+// } Driver Code Ends
