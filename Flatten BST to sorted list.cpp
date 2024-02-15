cf4a94a5af2bae4aedeef688147f5065482cc81a
Ayush Luthra <71180629+Ayushluthra2001@users.noreply.github.com>
2024-02-16 00:25:30 +0530

    Create Flatten BST to sorted list.cpp

---
+++ Flatten BST to sorted list.cpp
@@ -0,0 +1,187 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *left;
+    Node *right;
+    Node(int val)
+    {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+Node *newNode(int key)
+{
+    Node *node = new Node(key);
+    return node;
+}
+
+Node *buildTree(string str)
+{
+    // Corner Case
+    if (str.length() == 0 || str[0] == 'N')
+        return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for (string str; iss >> str;)
+        ip.push_back(str);
+
+    // Create the root of the tree
+    Node *root = newNode(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node *> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while (!queue.empty() && i < ip.size())
+    {
+
+        // Get and remove the front of the queue
+        Node *currNode = queue.front();
+        queue.pop();
+
+        // Get the current node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if (currVal != "N")
+        {
+
+            // Create the left child for the current node
+            currNode->left = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if (i >= ip.size())
+            break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if (currVal != "N")
+        {
+
+            // Create the right child for the current node
+            currNode->right = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+void printList(Node *head)
+{
+    while (head)
+    {
+        if(head->left)
+            cout<<"-1 ";
+        cout << head->data << " ";
+        head = head->right;
+    }
+    cout << "\n";
+}
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+/* Node of the binary search tree 
+class Node
+{
+public:
+    int data;
+    Node *left;
+    Node *right;
+    Node(int val)
+    {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+*/
+
+class Solution
+{
+public:
+void solve2(Node* root,vector<int>&temp){
+   
+    Node* t=root;
+    t->left=NULL;
+    t->right=NULL;
+    t->data=temp[0];
+    for(int i=1;i<temp.size();i++){
+        Node* tt=new Node(temp[i]);
+        
+       
+        tt->left=NULL;
+        t->left=NULL;
+        t->right=tt;
+        t=t->right;
+        
+    }
+// Node* k=t;
+// while(k!=NULL){
+//     cout<<k->data<<" ";
+//     k=k->right;
+// }
+// cout<<endl;
+    
+}
+void solve(Node* root , vector<int>&temp){
+    if(root==NULL) return;
+    solve(root->left,temp);
+    temp.push_back(root->data);
+    solve(root->right,temp);
+}
+    Node *flattenBST(Node *root)
+    {
+        // code here
+        vector<int>temp;
+        solve(root,temp);
+        solve2(root,temp);
+        return root;
+        
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    scanf("%d ", &t);
+    while (t--)
+    {
+        string str;
+        getline(cin, str);
+        Node *root = buildTree(str);
+        Solution ob;
+        auto ans = ob.flattenBST(root);
+        printList(ans);
+    }
+    return 0;
+}
+
+
+// } Driver Code Ends
