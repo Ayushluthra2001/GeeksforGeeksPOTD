63d9adc0d7ff2ebbba8f0eb734675bb22e017421
Ayush Luthra <71180629+Ayushluthra2001@users.noreply.github.com>
2024-03-02 11:29:24 +0530

    Create First element to occur k times.cpp

---
+++ First element to occur k times.cpp
@@ -0,0 +1,39 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+    public:
+    int firstElementKTime(int n, int k, int a[])
+    {
+        unordered_map<int,int>mapping;
+        for(int i=0;i<n;i++){
+            mapping[a[i]]++;
+            if(mapping[a[i]]>=k) return a[i];
+        }
+        
+        return -1;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+	int t;
+	cin >> t;
+	while(t--){
+	    int n, k;
+	    cin >> n >> k;
+	    int a[n];
+	    for(int i = 0;i<n;i++){
+	        cin >> a[i];
+	    }
+	    Solution ob;
+	    cout<<ob.firstElementKTime(n, k, a)<<endl;
+	}
+	
+	return 0;
+}
+// } Driver Code Ends
