530f43258484beca60c246a2367ac3c6bffa9521
Ayush Luthra <71180629+Ayushluthra2001@users.noreply.github.com>
2024-03-05 22:44:46 +0530

    Create Maximum Index(More optimize but stil TLE).cpp

---
+++ Maximum Index(More optimize but stil TLE).cpp
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+    public:
+        
+    // A[]: input array
+    // N: size of array
+    // Function to find the maximum index difference.
+    int maxIndexDiff(int a[], int n) 
+    { 
+        // Your code here
+        int maxi=0;
+        int start=0;
+        int end=n-1;
+        while(start<=end){
+            while(a[start]>a[end]) end--;
+            if(a[start]<=a[end]){
+                maxi=max(maxi,end-start);
+            }
+            start++;
+            end=n-1;
+        }
+        
+        
+       
+        return maxi;
+    }
+};
+
+//{ Driver Code Starts.
+  
+/* Driver program to test above functions */
+int main() 
+{
+    int T;
+    //testcases
+    cin>>T;
+    while(T--){
+        int num;
+        //size of array
+        cin>>num;
+        int arr[num];
+        
+        //inserting elements
+        for (int i = 0; i<num; i++)
+            cin>>arr[i];
+        Solution ob;
+        
+        //calling maxIndexDiff() function
+        cout<<ob.maxIndexDiff(arr, num)<<endl;    
+        
+    }
+    return 0;
+} 
+// } Driver Code Ends
