73d4e1ef6adc5b269bfb6816f72d2b11c6b5a744
Ayush Luthra <71180629+Ayushluthra2001@users.noreply.github.com>
2024-04-02 14:54:01 +0530

    Create Minimum Absolute Difference In BST.cpp

---
+++ Minimum Absolute Difference In BST.cpp
@@ -0,0 +1,158 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+  int data;
+  struct Node *left;
+  struct Node *right;
+};
+// Utility function to create a new Tree Node
+Node *newNode(int val)
+{
+  Node *temp = new Node;
+  temp->data = val;
+  temp->left = NULL;
+  temp->right = NULL;
+
+  return temp;
+}
+// Function to Build Tree
+Node *buildTree(string str)
+{
+  // Corner Case
+  if (str.length() == 0 || str[0] == 'N')
+    return NULL;
+
+  // Creating vector of strings from input
+  // string after spliting by space
+  vector<string> ip;
+
+  istringstream iss(str);
+  for (string str; iss >> str;)
+    ip.push_back(str);
+
+  // Create the root of the tree
+  Node *root = newNode(stoi(ip[0]));
+
+  // Push the root to the queue
+  queue<Node *> queue;
+  queue.push(root);
+
+  // Starting from the second element
+  int i = 1;
+  while (!queue.empty() && i < ip.size())
+  {
+
+    // Get and remove the front of the queue
+    Node *currNode = queue.front();
+    queue.pop();
+
+    // Get the current node's value from the string
+    string currVal = ip[i];
+
+    // If the left child is not null
+    if (currVal != "N")
+    {
+
+      // Create the left child for the current node
+      currNode->left = newNode(stoi(currVal));
+
+      // Push it to the queue
+      queue.push(currNode->left);
+    }
+
+    // For the right child
+    i++;
+    if (i >= ip.size())
+      break;
+    currVal = ip[i];
+
+    // If the right child is not null
+    if (currVal != "N")
+    {
+
+      // Create the right child for the current node
+      currNode->right = newNode(stoi(currVal));
+
+      // Push it to the queue
+      queue.push(currNode->right);
+    }
+    i++;
+  }
+
+  return root;
+}
+
+
+// } Driver Code Ends
+/*The Node structure is defined as
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+};
+*/
+
+class Solution
+{
+    
+    public:
+    void solve(vector<int>&ans,Node* root){
+        if(root==NULL) return;
+        solve(ans,root->left);
+        ans.push_back(root->data);
+        solve(ans,root->right);
+    }
+    int absolute_diff(Node *root)
+    {
+        
+        vector<int>ans;
+        solve(ans,root);
+        int mini=INT_MAX;
+        int start=0,end=ans.size()-1;
+        for(auto i : ans) cout<<i<< " ";
+        cout<<endl;
+        // while(start<end){
+        //     mini=min(mini,abs(ans[start]-ans[end]));
+        //     start++,end--;
+        // }
+        for(int i=0;i<ans.size();i++){
+            for(int j=i+1;j<ans.size();j++){
+                mini=min(mini,abs(ans[i]-ans[j]));
+            }
+        }
+        
+        return mini;
+        
+        
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main()
+{
+
+  int t;
+  scanf("%d ", &t);
+  while (t--)
+  {
+    string s;
+    getline(cin, s);
+    Node *root = buildTree(s);
+    // getline(cin, s);
+    if (root == NULL)
+      continue;
+    if (root->left == NULL and root->right == NULL)
+      continue;
+      Solution ob;
+    cout << ob.absolute_diff(root) << '\n';
+    // cout << s << '\n';
+  }
+  return 1;
+}
+// } Driver Code Ends
