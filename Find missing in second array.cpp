51b9a59ea16a8790cc101f9e0a889117c87b31f6
Ayush Luthra <71180629+Ayushluthra2001@users.noreply.github.com>
2024-04-19 10:24:22 +0530

    Create Find missing in second array.cpp

---
+++ Find missing in second array.cpp
@@ -0,0 +1,64 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+	
+	
+	public:
+	vector<int> findMissing(int a[], int b[], int n, int m) 
+	{ 
+	    // Your code goes here
+	    vector<int>ans;
+	   unordered_map<int,int>mapping;
+	   
+	        for(int j=0;j<m;j++){
+	           mapping[b[j]]++;
+	        }
+	       for(int i=0;i<n;i++){
+	           if(mapping.find(a[i])==mapping.end()){
+	               ans.push_back(a[i]);
+	           }
+	       }
+	    
+	    return ans;
+	} 
+};
+	
+
+//{ Driver Code Starts.
+
+int main() 
+{
+   	
+   
+   	int t;
+    cin >> t;
+    while (t--)
+    {
+    	int n;
+        cin>>n;
+        int m;
+        cin>>m;
+        int a[n];
+        int b[m];
+        for(int i=0;i<n;i++)
+        cin>>a[i];
+        for(int i=0;i<m;i++)
+        cin >> b[i];
+        vector<int> ans;
+        
+
+        Solution ob;
+        ans=ob.findMissing(a,b,n,m);
+        for(int i=0;i<ans.size();i++)
+        cout<<ans[i]<<" ";
+	    
+        
+	    cout << "\n";
+	     
+    }
+    return 0;
+}
+// } Driver Code Ends
