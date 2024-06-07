9285e49902bf6ee0f44d9d03165a4ba1bd6d3220
Ayush Luthra <71180629+Ayushluthra2001@users.noreply.github.com>
2024-06-08 02:01:41 +0530

    Create Index of an Extra Element.cpp

---
+++ Index of an Extra Element.cpp
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int findExtra(int n, int arr1[], int arr2[]) {
+        // add code here.
+        int i=0,j=0;
+        while(i<n && j<n){
+            if(arr1[i]!=arr2[j]){
+                return i;
+            }
+            i++,j++;
+        }
+        return i;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int arr1[n], arr2[n - 1];
+        for (int i = 0; i < n; i++) {
+            cin >> arr1[i];
+        }
+        for (int i = 0; i < n - 1; i++) {
+            cin >> arr2[i];
+        }
+        Solution obj;
+        cout << obj.findExtra(n, arr1, arr2) << endl;
+    }
+}
+// } Driver Code Ends
