eba59fbf84aeb63a57ee9e2f0c19070d1a1bfa7a
Ayush Luthra <71180629+Ayushluthra2001@users.noreply.github.com>
2024-06-19 21:52:44 +0530

    Create Find maximum volume of a cuboid.cpp

---
+++ Find maximum volume of a cuboid.cpp
@@ -0,0 +1,35 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    double maxVolume(double perimeter, double area) {
+        // code here
+           double h=(perimeter-sqrt(perimeter*perimeter-24*area))/12;
+        double l=(perimeter/4-2*h);
+        return h*h*l;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    long long int t;
+    cin >> t;
+    while (t--) {
+        double perimeter, area;
+        cin >> perimeter >> area;
+        // if (area == 5 and parameter == 15) {
+        //     cout << "0.46" << endl;
+        //     return 0;
+        // }
+        Solution ob;
+        double ans = ob.maxVolume(perimeter, area);
+        cout.precision(2);
+        cout << fixed << ans << "\n";
+    }
+}
+// } Driver Code Ends
