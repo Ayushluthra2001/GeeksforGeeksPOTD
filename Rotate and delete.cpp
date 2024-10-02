8065cf99c63f773967a631b4c06ae6b01b8a1099
Ayush Luthra <71180629+Ayushluthra2001@users.noreply.github.com>
2024-10-02 19:47:35 +0530

    Create Rotate and delete.cpp

---
+++ Rotate and delete.cpp
@@ -0,0 +1,43 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    int rotateDelete(vector<int> &arr) {
+        int n= arr.size();
+    int base_no = (((n-2)/4)*4)+2;
+    int base_value = (base_no/2)-(base_no/4-1);
+    return arr[(base_value+(n-base_no)%2)-1];
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution obj;
+        int res = obj.rotateDelete(arr);
+        cout << res << endl;
+        // string tl;
+        // getline(cin, tl);
+    }
+    return 0;
+}
+
+// } Driver Code Ends
