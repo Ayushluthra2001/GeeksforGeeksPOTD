867079f26a4d395b3eba923a374e7f64b4c90af3
Ayush Luthra <71180629+Ayushluthra2001@users.noreply.github.com>
2024-10-15 23:43:18 +0530

    Create Subarray range with given sum.cpp

---
+++ Subarray range with given sum.cpp
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    // Function to count the number of subarrays which adds to the given sum.
+    int subArraySum(vector<int>& arr, int tar) {
+        // Your code here
+        unordered_map<int,int>mapping;
+        
+        int sum = 0;
+        int ans = 0;
+        
+        for(int i : arr){
+            sum+=i;
+            
+            if(sum==tar) ans++;
+            if(mapping[sum-tar]) ans+=mapping[sum-tar];
+            mapping[sum]++;
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+    while (t--) {
+
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        string ks;
+        getline(cin, ks);
+        int tar = stoi(ks);
+        Solution obj;
+        int res = obj.subArraySum(arr, tar);
+        cout << res << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
