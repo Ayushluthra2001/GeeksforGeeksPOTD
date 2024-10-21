05f210a36f26f021559e7d3ec81537d18bb2cc5c
Ayush Luthra <71180629+Ayushluthra2001@users.noreply.github.com>
2024-10-21 16:24:20 +0530

    Create Split the Array.cpp

---
+++ Split the Array.cpp
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+class Solution {
+  public:
+    int countgroup(vector<int>& arr) {
+        // Complete the function
+        int mod = 1e9+7;
+        int x = 0;
+        for(int i =0 ;i<arr.size();i++){
+            x=x^arr[i];
+        }
+        if(x!=0) return 0;
+        return ((int)pow(2,arr.size()-1) % mod)-1;
+        
+        
+        
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution obj;
+        int ans = obj.countgroup(arr);
+        cout << ans << endl;
+    }
+}
+
+// } Driver Code Ends
