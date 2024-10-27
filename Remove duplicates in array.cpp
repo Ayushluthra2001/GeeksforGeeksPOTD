ffa713d6c3686fc7bc12a564fc84bed68001b7b5
Ayush Luthra <71180629+Ayushluthra2001@users.noreply.github.com>
2024-10-28 00:18:09 +0530

    Create Remove duplicates in array.cpp

---
+++ Remove duplicates in array.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    vector<int> removeDuplicate(vector<int>& arr) {
+        // code here
+        vector<int>ans;
+        
+        unordered_map<int,int>mapping;
+        for(auto i : arr){
+            if(mapping.find(i) == mapping.end())
+            ans.push_back(i);
+            mapping[i]++;
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution obj;
+        vector<int> ans = obj.removeDuplicate(arr);
+        for (int i = 0; i < ans.size(); i++) {
+            cout << ans[i] << " ";
+        }
+        cout << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
