f321cb2cff94c95aff6c12d40c235829b7dda047
Ayush Luthra <71180629+Ayushluthra2001@users.noreply.github.com>
2024-11-02 00:36:33 +0530

    Create Kth distance.cpp

---
+++ Kth distance.cpp
@@ -0,0 +1,61 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+
+class Solution {
+  public:
+
+    bool checkDuplicatesWithinK(vector<int>& arr, int k) {
+        // your code
+        unordered_map<int,vector<int>>mapping;
+        
+        for(int i = 0; i<arr.size(); i++){
+            mapping[arr[i]].push_back(i);
+        }
+        
+        for(auto i : mapping){
+            vector<int>temp = mapping[i.first];
+            
+            for(int i = 1;i<temp.size();i++){
+                if(temp[i]- temp[i-1] <=k) return true;
+            }
+        }
+        
+        return false;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        string ks;
+        getline(cin, ks);
+        int k = stoi(ks);
+        Solution obj;
+        bool res = obj.checkDuplicatesWithinK(arr, k);
+        if (res)
+            cout << "true" << endl;
+        else
+            cout << "false" << endl;
+        // cout << res << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
