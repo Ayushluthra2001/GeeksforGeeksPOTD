5e2a1a5a25de9ee55e60d13f109ae743b45ce91b
Ayush Luthra <71180629+Ayushluthra2001@users.noreply.github.com>
2024-11-07 20:34:07 +0530

    Create Split array in three equal sum subarrays.cpp

---
+++ Split array in three equal sum subarrays.cpp
@@ -0,0 +1,84 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+//  Class Solution to contain the method for solving the problem.
+class Solution {
+  public:
+    // Function to determine if array arr can be split into three equal sum sets.
+    vector<int> findSplit(vector<int>& arr) {
+        // code here
+        
+        int sum = 0;
+        for(auto i : arr) sum+=i;
+        if(sum%3!=0) return {-1,-1};
+        vector<int>ans;
+        int currSum =0;
+        int require = sum / 3;
+        for(int i = 0; i < arr.size();i++){
+            currSum += arr[i];
+            if(currSum == require){
+                ans.push_back(i);
+                currSum =0;
+            }
+            if(currSum > require) return {-1, -1};
+            if(ans.size()==2 ) return ans;
+        }
+        
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int test_cases;
+    cin >> test_cases;
+    cin.ignore();
+    while (test_cases--) {
+        string input;
+        vector<int> arr;
+
+        // Read the array from input line
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        // Solution instance to invoke the function
+        Solution ob;
+        vector<int> result = ob.findSplit(arr);
+
+        // Output result
+        if (result[0] == -1 && result[1] == -1 || result.size() != 2) {
+            cout << "false" << endl;
+        } else {
+            int sum1 = 0, sum2 = 0, sum3 = 0;
+            for (int i = 0; i < arr.size(); i++) {
+                if (i <= result[0])
+                    sum1 += arr[i];
+
+                else if (i <= result[1])
+                    sum2 += arr[i];
+
+                else
+                    sum3 += arr[i];
+            }
+            if (sum1 == sum2 && sum2 == sum3) {
+                cout << "true" << endl;
+            } else {
+                cout << "false" << endl;
+            }
+        }
+        cout << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
