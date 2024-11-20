0359465ca164917694131447b2957fd08e133ab5
Ayush Luthra <71180629+Ayushluthra2001@users.noreply.github.com>
2024-11-20 12:54:19 +0530

    Create Majority Element II.cpp

---
+++ Majority Element II.cpp
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    // Function to find the majority elements in the array
+    vector<int> findMajority(vector<int>& arr) {
+        // Your code goes here.
+        
+        vector<int>ans;
+        
+        int size = arr.size();
+        
+        map<int,int>mapping;
+        for(auto i: arr) mapping[i]++;
+        for(auto i : mapping) if(i.second >size/3) ans.push_back(i.first);
+        
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+
+        string s;
+        getline(cin, s);
+        stringstream ss(s);
+        vector<int> nums;
+        int num;
+        while (ss >> num) {
+            nums.push_back(num);
+        }
+        Solution ob;
+        vector<int> ans = ob.findMajority(nums);
+        if (ans.empty()) {
+            cout << "[]";
+        } else {
+            for (auto &i : ans)
+                cout << i << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
