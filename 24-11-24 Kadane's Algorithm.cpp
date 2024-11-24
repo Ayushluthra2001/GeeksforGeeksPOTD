3fb47eac2b48c72c9abc3b9d443f3ad30fdfbf42
Ayush Luthra <71180629+Ayushluthra2001@users.noreply.github.com>
2024-11-24 15:45:06 +0530

    Create 24-11-24 Kadane's Algorithm.cpp

---
+++ 24-11-24 Kadane's Algorithm.cpp
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+    // Function to find the sum of contiguous subarray with maximum sum.
+    int maxSubarraySum(vector<int> &arr) {
+        // code here...
+        int sum = 0;
+        int maxi = INT_MIN;
+        for(int i = 0; i<arr.size();i++){
+            sum += arr[i];
+            maxi = max(maxi , sum);
+            sum = sum < 0  ? 0 : sum;
+        }
+        return maxi;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore(); // To discard any leftover newline characters
+    while (t--)   // while testcases exist
+    {
+        vector<int> arr;
+        string input;
+        getline(cin, input); // Read the entire line for the array elements
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        Solution ob;
+        cout << ob.maxSubarraySum(arr) << endl;
+    }
+}
+// } Driver Code Ends
