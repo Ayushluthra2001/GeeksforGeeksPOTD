686da8980969cd2e02f8b9c8f702878282d3ee12
Ayush Luthra <71180629+Ayushluthra2001@users.noreply.github.com>
2024-12-04 20:27:00 +0530

    Create Strings Rotations of Each Other.cpp

---
+++ Strings Rotations of Each Other.cpp
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    // Function to check if two strings are rotations of each other or not.
+   bool areRotations(string &s1, string &s2) {
+        // Your code here
+         if(s1.length()!=s2.length()){
+           return false;
+       }else{
+           s1=s1+s1;
+           return (s1.find(s2)!=-1)?true:false;
+       }
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string s1;
+        string s2;
+        cin >> s1 >> s2;
+        Solution sol;
+        int result = sol.areRotations(s1, s2);
+        if (result) {
+            cout << "true" << endl;
+
+        } else {
+            cout << "false" << endl;
+        }
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
