df8f65056d02d470bb68eacec4fcc900ade3b6d0
Ayush Luthra <71180629+Ayushluthra2001@users.noreply.github.com>
2024-12-13 16:55:31 +0530

    Create Sorted and Rotated Minimum.cpp

---
+++ Sorted and Rotated Minimum.cpp
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int findMin(vector<int>& arr) {
+        // complete the function here
+        int mini  = INT_MAX;
+        for(auto i : arr) mini = min(mini , i);
+        return mini;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+
+    Solution ob;
+    while (t--) {
+        string line;
+        getline(cin, line);
+        stringstream ss(line);
+        vector<int> nums;
+        int num;
+        while (ss >> num) {
+            nums.push_back(num);
+        }
+        cout << ob.findMin(nums) << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
