191c6694ea91be2ed71b55a69c931f858295f8e4
Ayush Luthra <71180629+Ayushluthra2001@users.noreply.github.com>
2024-12-16 01:00:53 +0530

    Create K-th element of two Arrays(using ExtraSpace).cpp

---
+++ K-th element of two Arrays(using ExtraSpace).cpp
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int kthElement(vector<int>& a, vector<int>& b, int k) {
+        // code here
+        vector<int>third;
+        for(auto i : a) third.push_back(i);
+        for(auto i : b) third.push_back(i);
+        sort(third.begin(),third.end());
+        return third[k-1];
+    }
+};
+
+//{ Driver Code Starts.
+
+// Driver code
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        int n, m, k;
+        cin >> k;
+        cin.ignore();
+        string input;
+        int num;
+        vector<int> a, b;
+
+        getline(cin, input);
+        stringstream s2(input);
+        while (s2 >> num) {
+            a.push_back(num);
+        }
+
+        getline(cin, input);
+        stringstream s3(input);
+        while (s3 >> num) {
+            b.push_back(num);
+        }
+
+        Solution ob;
+        cout << ob.kthElement(a, b, k) << endl << "~\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
