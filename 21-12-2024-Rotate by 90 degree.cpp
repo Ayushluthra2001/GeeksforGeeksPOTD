91a33f9ab91464f482cd990bf075b10a6cd1128a
Ayush Luthra <71180629+Ayushluthra2001@users.noreply.github.com>
2024-12-21 00:18:17 +0530

    Create 21-12-2024-Rotate by 90 degree.cpp

---
+++ 21-12-2024-Rotate by 90 degree.cpp
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    // Function to rotate matrix anticlockwise by 90 degrees.
+    void rotateby90(vector<vector<int>>& mat) {
+        // code here
+        vector<vector<int>>temp = mat;
+        int k = 0 ,l=0;
+        for(int i = mat[0].size()-1 ;i>=0; i--){
+            for(int j = 0; j<mat.size();j++){
+                temp[k][l++] = mat[j][i];
+            }
+            k++;
+            l = 0;
+        }
+        mat = temp;
+        return  ;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<vector<int> > matrix(n);
+
+        for (int i = 0; i < n; i++) {
+            matrix[i].assign(n, 0);
+            for (int j = 0; j < n; j++) {
+                cin >> matrix[i][j];
+            }
+        }
+
+        Solution ob;
+        ob.rotateby90(matrix);
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j)
+                cout << matrix[i][j] << " ";
+            cout << endl;
+        }
+
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
