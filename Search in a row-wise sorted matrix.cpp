13fd857149a0b7c33646758578b9e1fd24ef50e0
Ayush Luthra <71180629+Ayushluthra2001@users.noreply.github.com>
2024-12-23 10:39:40 +0530

    Create Search in a row-wise sorted matrix.cpp

---
+++ Search in a row-wise sorted matrix.cpp
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+
+class Solution {
+  public:
+    // Function to search a given number in row-column sorted matrix.
+    bool searchRowMatrix(vector<vector<int>> &mat, int x) {
+        // code here
+        for(int i =0; i<mat.size();i++){
+            int start = 0 , end =  mat[0].size();
+            while(start <= end){
+                int mid = start + (end - start)/2;
+                if(mat[i][mid] == x) return true;
+                else if(mat[i][mid] > x) end = mid-1;
+                else start = mid + 1;
+            }
+        }
+        
+        return false;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+
+        int n, m;
+        cin >> n >> m;
+        vector<vector<int> > matrix(n);
+
+        for (int i = 0; i < n; i++) {
+            matrix[i].assign(m, 0);
+            for (int j = 0; j < m; j++) {
+                cin >> matrix[i][j];
+            }
+        }
+
+        int x;
+        cin >> x;
+        Solution obj;
+        if (obj.searchRowMatrix(matrix, x))
+            cout << "true\n";
+        else
+            cout << "false\n";
+
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
