6af6ee548c475412c8b08f168481ce2c7d51b797
Ayush Luthra <71180629+Ayushluthra2001@users.noreply.github.com>
2024-12-26 00:32:14 +0530

    Create Two Sum - Pair with Given Sum.cpp

---
+++ Two Sum - Pair with Given Sum.cpp
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+class Solution {
+  public:
+    bool twoSum(vector<int>& arr, int target) {
+        // code here
+        unordered_map<int,int>mapping;
+        for(auto i : arr){
+            int rem = target - i;
+            if(mapping.find(rem)!= mapping.end()) return true;
+            else mapping[i]++;
+        }
+        return false;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        int x;
+        cin >> x;
+        cin.ignore();
+
+        vector<int> arr;
+        string input;
+        getline(cin, input); // Read the entire line for the array elements
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        Solution ob;
+        auto ans = ob.twoSum(arr, x);
+        cout << (ans ? "true" : "false") << endl;
+        cout << "~" << endl;
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
