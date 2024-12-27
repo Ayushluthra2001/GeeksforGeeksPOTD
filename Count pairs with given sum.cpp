f9ca9f15f74f9e5753838ce42e6a688e204b7996
Ayush Luthra <71180629+Ayushluthra2001@users.noreply.github.com>
2024-12-27 16:32:07 +0530

    Create Count pairs with given sum.cpp

---
+++ Count pairs with given sum.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int countPairs(vector<int> &arr, int target) {
+        // Code here
+        int count =0;
+        unordered_map<int,int>mapping;
+        for(int i =0; i<arr.size();i++){
+            int rem = target - arr[i];
+            if(mapping.find(rem) != mapping.end()) {
+                count+= mapping[rem];
+            }
+            mapping[arr[i]]++;
+        }
+        return count;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        int target;
+        cin >> target;
+        cin.ignore();
+        Solution ob;
+        int res = ob.countPairs(arr, target);
+
+        cout << res << endl << "~" << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
