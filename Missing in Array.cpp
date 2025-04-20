cedb03d1b9041ef2bc7cca3a664906bd4b6d881f
Ayush Luthra <71180629+Ayushluthra2001@users.noreply.github.com>
2025-04-21 01:37:29 +0530

    Create Missing in Array.cpp

---
+++ Missing in Array.cpp
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    int missingNum(vector<int>& arr) {
+        // code here
+        int xorr = arr[0];
+        for(int i = 1; i<arr.size(); i++){
+            xorr  = xorr ^ arr[i];
+            
+        }
+        for(int i = 1;i<=arr.size()+1;i++) xorr = xorr ^ i;
+        return xorr;
+    }
+};
+   
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore(); // to ignore the newline after the integer input
+    while (t--) {
+        int n;
+        vector<int> a;
+        string input;
+
+        // Input format: first number n followed by the array elements
+        getline(cin, input);
+        stringstream ss(input);
+        int num;
+        while (ss >> num)
+            a.push_back(num);
+
+        Solution obj;
+        cout << obj.missingNum(a) << endl;
+        cout << "~\n";
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
