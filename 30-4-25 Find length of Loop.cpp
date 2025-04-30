cca25ea7e484664fb4e3a4563aff7977b7189040
Ayush Luthra <71180629+Ayushluthra2001@users.noreply.github.com>
2025-04-30 14:06:17 +0530

    Create 30-4-25 Find length of Loop.cpp

---
+++ 30-4-25 Find length of Loop.cpp
@@ -0,0 +1,101 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    struct Node *next;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+void loopHere(Node *head, Node *tail, int position) {
+    if (position == 0)
+        return;
+
+    Node *walk = head;
+    for (int i = 1; i < position; i++)
+        walk = walk->next;
+    tail->next = walk;
+}
+
+
+// } Driver Code Ends
+
+/*
+struct Node {
+    int data;
+    struct Node *next;
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+*/
+class Solution {
+  public:
+    // Function to find the length of a loop in the linked list.
+    int countNodesinLoop(Node *head) {
+        // Code here
+        Node* slow = head;
+        Node* fast = head->next;
+        while(slow != NULL && fast!= NULL && slow != fast) {
+            slow = slow->next;
+            fast = fast->next;
+            
+            if(fast != NULL) fast = fast->next;
+            
+        }
+        if(slow == NULL || fast==NULL) return 0;
+        
+        Node* temp = slow;
+        slow = slow->next;
+        int count = 1;
+        while(temp!= slow) {
+            slow = slow->next;
+            count++;
+        }
+        return count;
+        
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        int k;
+        cin >> k;
+        cin.ignore();
+        struct Node *head = new Node(arr[0]);
+        struct Node *tail = head;
+        for (int i = 1; i < arr.size(); ++i) {
+            tail->next = new Node(arr[i]);
+            tail = tail->next;
+        }
+        loopHere(head, tail, k);
+
+        Solution ob;
+        cout << ob.countNodesinLoop(head) << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
