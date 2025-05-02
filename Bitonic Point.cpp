3e4a133b460483db618faab0cf631f49ebc380e1
Ayush Luthra <71180629+Ayushluthra2001@users.noreply.github.com>
2025-05-02 19:48:24 +0530

    Create Bitonic Point.cpp

---
+++ Bitonic Point.cpp
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+// User function template for C++
+class Solution {
+  public:
+    int findMaximum(vector<int> &arr) {
+        // code here
+        int Bpoint = 0;
+        for(int i = 1; i<arr.size();i++){
+            if(arr[i-1] < arr[i]){
+                Bpoint = i;
+            }else return arr[i-1];
+        }
+        
+        return arr[Bpoint];
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+
+    while (t--) {
+        vector<int> arr;
+        int value;
+
+        // Read the entire line of integers
+        string line;
+        // cin.ignore(); // Ignore the newline after the test case input
+        getline(cin, line);
+        stringstream ss(line);
+
+        // Parse integers and add them to the vector
+        while (ss >> value) {
+            arr.push_back(value);
+        }
+
+        Solution ob;
+        auto ans = ob.findMaximum(arr);
+        cout << ans << "\n";
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
