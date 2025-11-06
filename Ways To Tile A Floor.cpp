dc5a8e059fab65542442d9c8ef9c41a9ffd96779
Ayush Luthra <71180629+Ayushluthra2001@users.noreply.github.com>
2025-11-06 20:33:09 +0530

    Create Ways To Tile A Floor.cpp

---
+++ Ways To Tile A Floor.cpp
@@ -0,0 +1,18 @@
+class Solution {
+  public:
+    int numberOfWays(int n) {
+        // code here
+        if(n==1) return 1;
+        if(n==2) return 2;
+        int a = 1;
+        int b = 2;
+        int c = 0;
+        for(int i =3 ;i<=n; i++){
+            c = a + b;
+            a = b;
+            b = c;
+            
+        }
+        return c;
+    }
+};
